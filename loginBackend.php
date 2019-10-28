de80931bb5abffddd617bcffc2cdd57a35ac6acf
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-10-28 17:40:31 -0500

    fixed syntax errors but SQL connection error exists

--- loginBackend.php
+++ loginBackend.php
@@ -2,7 +2,7 @@
 $servername = "mysql.eecs.ku.edu";
 $username = "z181k532";
 $password = "jo9aiyaa";
-$dbName = "z181k532"
+$dbName = "z181k532";
 
 $connect = new mysqli($servername, $username, $password, $dbName);
 if($connect->connect_error)
@@ -11,20 +11,19 @@ if($connect->connect_error)
   }
 echo "Connected successfully";
 
-$sql = "CREATE TABLE usernames
-(
+$sql = "CREATE TABLE usernames (
   ID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
 )";
 
-$webUser = $connect->mysqli_real_escape_string($_REQUEST['username'];
-$webPass = $connect->mysqli_real_escape_string($_REQUEST['password'];
+$webUser = $connect->mysqli_real_escape_string($_REQUEST['username']);
+$webPass = $connect->mysqli_real_escape_string($_REQUEST['password']);
 
 $sql = "INSERT INTO usernames (username, password) VALUES ('$webUser','$webPass')";
 if($connect->query($sql) === true)
   {
-    echo "Records inserted"
+    echo "Records inserted";
   }
 else {
   echo "Could not insert " . $connect->error;
