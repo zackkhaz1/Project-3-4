f39072c802057d66ea752fd3d8fecbd7ee6d9f53
Cobb <hacobb@ku.edu>
2019-11-01 12:24:24 -0500

    adding most html files needed and some php files. mostly empty shells at this moment.

---
+++ createTeam.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "mysql.eecs.ku.edu";
+$username = "zack_khaz";
+$password = "jo9aiyaa";
+$dbName = "zack_khaz";
+
+$connect = new mysqli($servername, $username, $password, $dbName);
+if($connect->connect_error)
+  {
+    die("Connection failed: " . $connect->connect_error);
+  }
+else {
+echo "Connected successfully <br>";
+}
+
+
+$connect->close();
+
+ ?>
